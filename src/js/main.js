160fcb2ce43b1ce3f5f372f6860b2963f61d5c04
Marty <Marty2009@yandex.ru>
2023-01-03 17:34:28 +0300

    сделал видео шапку

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
-
+//  Включение автоматического воспроизведения видео
+const videoLocation  = document.querySelector('.video-header__video');
+window.onload = addAutoplay();
+function addAutoplay() {
+  if (videoLocation) {
+    if(window.innerWidth > 1020){
+      videoLocation.setAttribute("preload", "");
+      videoLocation.setAttribute("autoplay","");
+    };
+  }
+}
