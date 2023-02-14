831b4eff38c694ecd14c3fa5786d7d674ecf984f
Marty <Marty2009@yandex.ru>
2023-02-14 23:28:36 +0300

    сделал страницу новостей без фильтра по дате

--- src/js/news.js
+++ src/js/news.js
@@ -38,3 +38,41 @@ if (newsSwiperItem) {
 
   const swiper = new Swiper(newsSwiperItem, swiperOptions);
 }
+
+const newsFilterFormItem = document.querySelector('#news-filter-form');
+
+if (newsFilterFormItem) {
+  const dateFilterItem = newsFilterFormItem.querySelector('#news-date-filter');
+  const choice = new Choices(dateFilterItem, {
+    placeholder: false,
+    searchEnabled: false,
+    shouldSort: false,
+    itemSelectText: '',
+    classNames: {
+      containerOuter: 'form-choices',
+      containerInner: 'form-choices__inner',
+      input: 'form-choices__input',
+      inputCloned: 'form-choices__input--cloned',
+      list: 'form-choices__list',
+      listItems: 'form-choices__list--multiple',
+      listSingle: 'form-choices__list--single',
+      listDropdown: 'form-choices__list--dropdown',
+      item: 'form-choices__item',
+      itemSelectable: 'form-choices__item--selectable',
+      itemDisabled: 'form-choices__item--disabled',
+      itemOption: 'form-choices__item--choice',
+      group: 'choices__group',
+      groupHeading : 'choices__heading',
+      button: 'choices__button',
+      activeState: 'is-active',
+      focusState: 'is-focused',
+      openState: 'is-open',
+      disabledState: 'is-disabled',
+      highlightedState: 'is-highlighted',
+      selectedState: 'is-selected',
+      flippedState: 'is-flipped'
+    },
+    allowHTML: true
+    // callbackOnInit: filterNews
+  });
+}
