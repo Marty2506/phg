addd4d6561e2fb8cab4b9f0bbd57cafe8b209267
Marty <Marty2009@yandex.ru>
2023-02-22 22:39:02 +0300

    сделал боковую панельку

--- src/js/global.js
+++ src/js/global.js
@@ -1,9 +1,10 @@
-const menu = document.querySelector('.menu');
-const pageHeader = document.querySelector('.page-header');
+const menu = document.querySelector(".menu");
+const pageHeader = document.querySelector(".page-header");
+const SCROLL_SIZE = 1000;
 
 /**
  * Проверяет, что нажатая клавиша соответствует клавише Escape
  * @param {object} evt
  * @returns {boolean} true, если Escape
  */
-const isEscapeKey = (evt) => evt.key === 'Escape';
+const isEscapeKey = (evt) => evt.key === "Escape";
---
+++ src/js/side-panel.js
@@ -0,0 +1,39 @@
+const sidePanel = document.querySelector(".side-panel");
+
+if (sidePanel) {
+  const tariffButton = sidePanel.querySelector("#side-panel-tariff");
+  const tariffSection = document.querySelector("#tariffs");
+  const scheduleSection = document.querySelector("#schedule");
+  const scrollUpButton = sidePanel.querySelector("#side-panel-to-top");
+  const scheduleButton = sidePanel.querySelector("#side-panel-schedule");
+
+  if (tariffSection) {
+    tariffButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      tariffSection.scrollIntoView({ behavior: "smooth" });
+    });
+  }
+
+  if (scheduleSection) {
+    scheduleButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      console.log("clicked");
+      scheduleSection.scrollIntoView({ behavior: "smooth" });
+    });
+  }
+
+  scrollUpButton.addEventListener("click", (evt) => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+
+  window.addEventListener("scroll", (evt) => {
+    if (
+      document.body.scrollTop > SCROLL_SIZE ||
+      document.documentElement.scrollTop > SCROLL_SIZE
+    ) {
+      sidePanel.classList.add("side-panel--active");
+    } else {
+      sidePanel.classList.remove("side-panel--active");
+    }
+  });
+}
